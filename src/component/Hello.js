bf52604b8e12d4cea649de2839c4bceda5d8d3b0
Benjamin Lepine <benjamin.lepine@epitech.eu>
2018-09-21 00:01:33 +0200

    begin front

---
+++ src/component/Hello.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Weatherapp extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="weekcontainer">
+                    <div className="weekcontainer1">1</div>
+                    <div className="weekcontainer2">2</div>
+                    <div className="weekcontainer1">3</div>
+                    <div className="weekcontainer2">4</div>
+                    <div className="weekcontainer1">5</div>
+                    <div className="weekcontainer2">6</div>
+                    <div className="weekcontainer1">7</div>
+                </div>
+                <div className="weekcontainerday">
+                    <div className="weekcontainerday1">MON</div>
+                    <div className="weekcontainerday2">TUE</div>
+                    <div className="weekcontainerday1">WED</div>
+                    <div className="weekcontainerday2">THU</div>
+                    <div className="weekcontainerday1">FRI</div>
+                    <div className="weekcontainerday2">SAT</div>
+                    <div className="weekcontainerday1">SUN</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Weatherapp;
+
