9d5cf9a177285bd56c4423b5e40661351b2afafb
Benjamin Lepine <benjamin.lepine@epitech.eu>
2018-12-08 18:20:11 +0100

    commit all cards

---
+++ src/component/citycard.js
@@ -0,0 +1,37 @@
+/**
+ * Created by benjamin on 06/11/2018.
+ */
+import React, { Component } from 'react';
+import '../App.css';
+
+ class Citycard extends React.Component {
+
+
+
+    render(){
+        return (
+            <div className="card inlineblockleft">
+                <div className="block">
+                    <div className="city"><span>{this.props.city}</span></div>
+                    <div className="mainimage"></div>
+                    <div className="degree"><span>20°</span></div>
+                    <div className="weather"><span>CLOUDY</span></div>
+                </div>
+                <div className="temperaturemain">
+                    <div className="inlineblockleft">
+                        <div className="cold"></div>
+                        <div className="temperature"><span>18</span></div>
+                        <div className="coldtext"><span>Min</span></div>
+                    </div>
+                    <div className="inlineblockright">
+                        <div className="hot"></div>
+                        <div className="temperature"><span>26</span></div>
+                        <div className="hottext"><span>max</span></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Citycard;
